ae3b973a3b45ee05dde6f212d68b3a7fcb626ac3
SG master <1363028533@qq.com>
2017-11-02 00:04:13 +0800

    Update

---
+++ java/supergp/cskiller/Killer/ZRS.java
@@ -0,0 +1,10 @@
+package supergp.cskiller.Killer;
+
+import supergp.cskiller.Commander;
+
+public class ZRS extends Commander {
+    public ZRS(int n, int seat) {
+        super(n, seat);
+    }
+
+}
