5184887c93c2525d5c9da9b72f9977464b37f298
SG master <1363028533@qq.com>
2017-11-02 00:04:08 +0800

    Update

---
+++ java/supergp/cskiller/Killer/GTX.java
@@ -0,0 +1,10 @@
+package supergp.cskiller.Killer;
+
+import supergp.cskiller.Commander;
+
+public class GTX extends Commander {
+    public GTX(int n, int seat) {
+        super(n, seat);
+    }
+
+}
